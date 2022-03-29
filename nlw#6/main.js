1f5c4df16af5e8247257f6d867e98d9b58bc8a41
Guilherme Tarifa <guilherme.vieira@sptech.school>
2022-03-29 18:40:40 -0300

    Site institucional feita com HTML/CSS/JS

---
+++ nlw#6/main.js
@@ -0,0 +1,62 @@
+// click do menu //
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// esconder menu quando clicar em algum item da lista //
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// mudar o shadow do header, qnd der o scroll //
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que o header//
+    header.classList.add('scroll')
+  } else {
+    //menor q altura do header//
+    header.classList.remove('scroll')
+  }
+})
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// ScrollReveal: Mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
+
+// loading //
